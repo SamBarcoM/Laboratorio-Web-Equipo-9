3201afa280302749e22fb3b6d782ee529fac5e67
Emilio Cantón <eecanton@gmail.com>
2020-09-07 20:28:44 -0500

    feat: Add H3 Chatbot front and back

--- chatbot-html/src/App.js
+++ chatbot-html/src/App.js
@@ -4,11 +4,21 @@ import Home from "./Pages/Home"
 import User from "./Pages/User"
 import LandingPage from "./Pages/LandingPage"
 import NavBar from "./Components/NavBar"
-import CustomChatbot from "./Components/CustomChatbot"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// import CustomChatbot from "./Components/CustomChatbot"
+import 'react-chat-widget/lib/styles.css';
+import { Widget, addResponseMessage } from "react-chat-widget";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import * as axios from 'axios';
 
 function App() {
+	const handleNewUserMessage = async (message) => {
+		const { data } = await axios.post('http://127.0.0.1:5002/getMessage', {
+			message
+		});
+
+		addResponseMessage(data.text);
+	}
 
   return (
     <Router>
@@ -24,11 +34,11 @@ function App() {
           <LandingPage />
         </Route>
       </Switch>
-      <CustomChatbot />
-      {/*<Widget handleNewUserMessage={handleNewUserMessage}
-        title="TEC"
-        subtitle= "Hola, escribe tus dudas"
-      />*/} 
+      
+      <Widget handleNewUserMessage={handleNewUserMessage}
+        title="Chatbot"
+        subtitle= "Hola! Esta es la tarea 3."
+      />
       
     </Router>
   );
