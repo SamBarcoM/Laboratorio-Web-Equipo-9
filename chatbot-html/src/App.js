1be653dc163f856b77107123278c870dd35f697d
Kyungtak Woo <kevinwkt@kaero.localdomain>
2020-09-14 08:19:48 -0500

    fix: render html with custom component using renderComponent from react-chat-widget

--- chatbot-html/src/App.js
+++ chatbot-html/src/App.js
@@ -4,12 +4,24 @@ import Home from "./Pages/Home"
 import User from "./Pages/User"
 import LandingPage from "./Pages/LandingPage"
 import NavBar from "./Components/NavBar"
-// import CustomChatbot from "./Components/CustomChatbot"
-import 'react-chat-widget/lib/styles.css';
-import { Widget, addResponseMessage } from "react-chat-widget";
+import ReactHtmlParser from "react-html-parser";
+import { Widget, renderCustomComponent } from "react-chat-widget";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import * as axios from 'axios';
+
+import 'react-chat-widget/lib/styles.css';
+import './Components/chat.css';
+
+class CustomComponent extends React.Component {
+  render() {
+    return (
+      <div className="speech-bubble">
+        {ReactHtmlParser(this.props.custom)}
+      </div>
+    );
+  }
+}
 
 function App() {
 	const handleNewUserMessage = async (message) => {
@@ -17,7 +29,7 @@ function App() {
 			message
 		});
 
-		addResponseMessage(data.text);
+    renderCustomComponent(CustomComponent, { custom: data.text} );
 	}
 
   return (
@@ -34,7 +46,7 @@ function App() {
           <LandingPage />
         </Route>
       </Switch>
-      
+      {/* <CustomChatbot />  */}
       <Widget handleNewUserMessage={handleNewUserMessage}
         title="Chatbot"
         subtitle= "Hola! Esta es la tarea 3."
